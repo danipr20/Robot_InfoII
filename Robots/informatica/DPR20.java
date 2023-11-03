e8a846f50a54af5f3143c1a2be4bded0cc51c2be
Daniel <danipr2000@gmail.com>
2023-11-03 16:30:15 +0100

    modificación de Robot DPR20, le doy mejor formato, para mayor claridad visual y comprensión en el codigo fuente (hay que comprobar que el robot funcione correctamente)

--- Robots/informatica/DPR20.java
+++ Robots/informatica/DPR20.java
@@ -1,194 +1,155 @@
 package informatica;
+
 import robocode.*;
 import java.awt.Color;
 import static robocode.util.Utils.normalRelativeAngleDegrees;
 
+public class DPR20 extends Robot {
+	// Variables de estado
+	boolean enemiDetected = false;
+	boolean posicionamiento = false;
+	boolean attack = false;
+	double myEnergy, myHeading, myGunHeading, myX, myY, enemyVelocity, gunTurnAmt;
+	String enemyName = "0";
+	int bonusFire = 0;
+	int bonusRadar = 0;
+	int bonusMov = 0;
 
+	public void run() {
+		// Configuración de colores
+		setBodyColor(Color.black);
+		setRadarColor(Color.red);
+		setGunColor(Color.white);
+		setBulletColor(Color.red);
 
-public class DPR20 extends Robot{
-    /**
-     * run: DPR20's default behavior
-     */
-		boolean enemiDetected=false;	boolean posicionamiento=false;	boolean attack=false;
-		double myEnergy, myHeading, myGunHeading, myX, myY,enemyVelocity,gunTurnAmt;
-		String enemyName="0";
-		int bonusFire=0; int bonusRadar=0; int bonusMov=0;
-    public void run() {
-	    setBodyColor(Color.black);
-        setRadarColor(Color.red);
-        setGunColor(Color.white);
-        setBulletColor(Color.red);
-establecerEnemigo();
-		int a=1;
+		establecerEnemigo(); // Ajusta la estrategia según el enemigo
+		int a = 1;
 
-	takeCenter();
-	
-		
-if(posicionamiento){enemiDetected=false;attack=false;}
+		takeCenter(); // Posiciona el robot en el centro del campo de batalla
 
-		
-
-	
-
-		
-        while (true) {
-		System.out.println("enemi:"+enemyName);	System.out.println("fire bonus: " + bonusFire);
-		establecerEnemigo();
-		if(!enemiDetected){spotEnemy(a,bonusRadar);}
-		if(enemiDetected){a=-a;}
-		enemiDetected=false;
-		if(getEnergy()<40){myRobotInfo();}
-		
-        }}
-
-    /**
-     * onScannedRobot: What to do when you see another robot
-     */
-    public void onScannedRobot(ScannedRobotEvent e) {
-	enemiDetected=true;
-	enemyName=e.getName();
-		enemyVelocity=e.getVelocity();
-		
- 	if(enemyName.equals("sample.PaintingRobot")||(!enemyName.equals("sample.PaintingRobot")&&!enemyName.equals("sample.Crazy")&&!enemyName.equals("sample.Corners")&&!enemyName.equals("0"))){
-	if (e.getDistance() > 150) {
-			gunTurnAmt = normalRelativeAngleDegrees(e.getBearing() + (getHeading() - getRadarHeading()));
-
-			turnGunRight(gunTurnAmt); // Try changing these to setTurnGunRight,
-			turnRight(e.getBearing()); // and see how much Tracker improves...
-			// (you'll have to make Tracker an AdvancedRobot)
-			ahead(e.getDistance() - 140);
-			return;
+		if (posicionamiento) {
+			enemiDetected = false;
+			attack = false;
 		}
 
-		// Our target is close.
-		gunTurnAmt = normalRelativeAngleDegrees(e.getBearing() + (getHeading() - getRadarHeading()));
-		turnGunRight(gunTurnAmt);
-		fire(3);
+		while (true) {
+			establecerEnemigo(); // Actualiza la estrategia según el enemigo
+			if (!enemiDetected) {
+				spotEnemy(a, bonusRadar); // Gira el radar para buscar al enemigo
+			}
+			if (enemiDetected) {
+				a = -a; // Cambia la dirección del radar cuando se detecta al enemigo
+			}
+			enemiDetected = false;
+		}
+	}
 
-		// Our target is too close!  Back up.
-		if (e.getDistance() < 100) {
-			if (e.getBearing() > -90 && e.getBearing() <= 90) {
-				back(40);
-			} else {
-				ahead(40);
+	public void onScannedRobot(ScannedRobotEvent e) {
+		enemiDetected = true;
+		enemyName = e.getName();
+		enemyVelocity = e.getVelocity();
+		if (enemyName.equals("sample.Crazy") || enemyName.equals("sample.Corners")) {
+			fireI(e.getDistance());
+		} // Decisión de cuánta energía usar para disparar
+		else {
+			if (e.getDistance() > 150) {
+				gunTurnAmt = normalRelativeAngleDegrees(e.getBearing() + (getHeading() - getRadarHeading()));
+				turnGunRight(gunTurnAmt); // Apunta al enemigo
+				turnRight(e.getBearing()); // Gira para enfrentar al enemigo
+				ahead(e.getDistance() - 140); // Avanza hacia el enemigo
+				return;
+			}
+
+			gunTurnAmt = normalRelativeAngleDegrees(e.getBearing() + (getHeading() - getRadarHeading()));
+			turnGunRight(gunTurnAmt);
+			fireI(e.getDistance()); // Dispara al enemigo
+
+			if (e.getDistance() < 100) {
+				// Retrocede si el enemigo está muy cerca
+				if (e.getBearing() > -90 && e.getBearing() <= 90) {
+					back(40);
+				} else {
+					ahead(40);
+				}
 			}
 		}
-		scan();
 	}
 
-	fireI(e.getDistance());
-
-	System.out.println("Energy: " + e.getEnergy());
-    System.out.println("Distance: " + e.getDistance());
-	System.out.println("real"+e.getName());
-	System.out.println("var"+enemyName);
-	System.out.println("fire bonus: " + bonusFire);
-	System.out.println("vel: " + enemyVelocity);
-    }
-
-    /**
-     * onHitByBullet: What to do when you're hit by a bullet
-     */
-    public void onHitByBullet(HitByBulletEvent e) {
-	
-	 if(!posicionamiento&&(enemyName.equals("sample.Corners")||enemyName.equals("sample.Crazy"))){
-				turnLeft(90);
-				ahead(bonusMov);
-}
-    }
-    
-    /**
-     * onHitWall: What to do when you hit a wall
-     */
-    public void onHitWall(HitWallEvent e) {
-        takeCenter();
-    }
-    
-    public void takeCenter() {	
-double startheading=getHeading();
-//Al empezar nos posicionamos y tomamos el centro
-		posicionamiento=true;	
-		if((startheading<=180)){turnLeft(startheading);}
-		else{turnRight(360-startheading);}
-		ahead((getBattleFieldHeight()/2)-getY());
-		turnRight(90);
-		ahead((getBattleFieldWidth()/2)-getX());
-		posicionamiento=false;
-    }
-    
-    public void spotEnemy(int a, int bonusRadar) {
-	int i=10+bonusRadar; 	
-			i=i*a;
-			turnGunRight(i);
-				}
-
-
-      public void myRobotInfo() {	
-		myEnergy=	getEnergy();
-myHeading= 	getHeading();
-myGunHeading= 	getGunHeading();
-myX=getX();
-myY=getY();
-
-		
-    }       	
-			
-public void establecerEnemigo(){
-	if (enemyName.equals("sample.Corners")) {//parametros optimizados 2,15,75
-    bonusFire = 2;
-    bonusRadar = 15;
-	bonusMov=75;
-	
-} else if (enemyName.equals("sample.Crazy")) {
-    bonusFire = 0;
-    bonusRadar = 25;
-	bonusMov=40;
-	
-} else if (enemyName.equals("sample.PaintingRobot")) {
-    bonusFire = 0;
-    bonusRadar = 10;
-	bonusMov=25;
-	
-}  else  if (!enemyName.equals("sample.Crazy")&&!enemyName.equals("sample.Crazy")&&!enemyName.equals("sample.PaintingRobot")&&!enemyName.equals("0")) {
-    bonusFire = 1;
-    bonusRadar = 15;
-	bonusMov=10;
-	
-}else  if(enemyName.equals("0")){
-    bonusFire = 0;
-    bonusRadar = 35;
-	bonusMov=25;
-}
-}
-
-
-	 public void fireI(double robotDistance) {
-
-	if (robotDistance > 300 || getEnergy() < 20) {
-			fire(1+bonusFire);
-		} else if (robotDistance > 75) {
-			fire(2+bonusFire);
-		} else {
-			fire(3+bonusFire);
+	public void onHitByBullet(HitByBulletEvent e) {
+		// En caso de ser impactado por una bala
+		if (!posicionamiento && (enemyName.equals("sample.Corners") || enemyName.equals("sample.Crazy"))) {
+			turnLeft(90); // Gira 90 grados
+			ahead(bonusMov); // Avanza según la estrategia
 		}
 	}
-		
- 
-    
 
+	public void onHitWall(HitWallEvent e) {
+		takeCenter(); /*
+						 * Reposicionam el robot en el centro del campo de batalla al
+						 * chocar contra una pared
+						 */
+	}
+
+	public void takeCenter() {
+		double startheading = getHeading();
+		posicionamiento = true;
+		if (startheading <= 180) {
+			turnLeft(startheading);
+		} else {
+			turnRight(360 - startheading);
+		}
+		ahead((getBattleFieldHeight() / 2) - getY());
+		turnRight(90);
+		ahead((getBattleFieldWidth() / 2) - getX());
+		posicionamiento = false;
+	}
+
+	public void spotEnemy(int a, int bonusRadar) {
+		int i = 10 + bonusRadar;
+		i = i * a;
+		turnGunRight(i); // Gira el radar en sentido de "a"
+	}
+
+	public void myRobotInfo() {
+		myEnergy = getEnergy();
+		myHeading = getHeading();
+		myGunHeading = getGunHeading();
+		myX = getX();
+		myY = getY();
+	}
+
+	public void establecerEnemigo() {
+		if (enemyName.equals("sample.Corners")) {
+			bonusFire = 2;
+			bonusRadar = 15;
+			bonusMov = 75;
+		} else if (enemyName.equals("sample.Crazy")) {
+			bonusFire = 0;
+			bonusRadar = 25;
+			bonusMov = 40;
+		} else if (enemyName.equals("sample.PaintingRobot")) {
+			bonusFire = 0;
+			bonusRadar = 10;
+			bonusMov = 25;
+		} else if (!enemyName.equals("sample.Crazy") && !enemyName.equals("sample.Crazy")
+				&& !enemyName.equals("sample.PaintingRobot") && !enemyName.equals("0")) {
+			bonusFire = 1;
+			bonusRadar = 15;
+			bonusMov = 10;
+		} else if (enemyName.equals("0")) {
+			bonusFire = 0;
+			bonusRadar = 35;
+			bonusMov = 25;
+		}
+	}
+
+	public void fireI(double robotDistance) {
+		if (robotDistance > 300 || getEnergy() < 20) {
+			fire(1 + bonusFire); // Dispara con potencia 1 + bonus
+		} else if (robotDistance > 75) {
+			fire(2 + bonusFire); // Dispara con potencia 2 + bonus
+		} else {
+			fire(3 + bonusFire); // Dispara con potencia 3 + bonus
+		}
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
